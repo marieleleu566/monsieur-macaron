8819c4bb6fe81aaac3cedebbadd3fa414cd91268
Marie Leleu <marieleleu@icloud.com>
2021-02-05 17:57:57 +0100

    added npm map and altered pages

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,17 @@ Rails.application.configure do
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+config.serve_static_assets = false
+
+# Compress JavaScripts and CSS
+config.assets.compress = true
+
+# Don't fallback to assets pipeline if a precompiled asset is missed
+config.assets.compile = false
+
+# Generate digests for assets URLs
+config.assets.digest = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
