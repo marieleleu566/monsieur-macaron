5f4876ccdc52e4078261757b962e609ed3363f77
Marie Leleu <marieleleu@icloud.com>
2021-02-12 13:03:46 +0100

    made media responsive for iphone and ipad

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Perfected blend of French art and baking.", "Proudly based in Hawke's Bay.", "We deliver across the Nth. Island.", "Perfected blend of French art and baking."],
+    strings: ["Perfected blend of French art and baking.", "Proudly based in Hawke's Bay.", "We deliver across the Nth. Island."],
     typeSpeed: 50,
     loop: true
   });
