f906125c00323915cec30b21fb7f5a1d026b9223
Marie Leleu <marieleleu@icloud.com>
2021-03-13 19:02:21 +0100

    test

--- config/initializers/recaptcha.rb
+++
@@ -1,4 +0,0 @@
-Recaptcha.configure do |config|
-  config.site_key   = Rails.application.config_for(:recaptcha)[ENV["RECAPTCHA_SITE_KEY"]]
-  config.secret_key = Rails.application.config_for(:recaptcha)ENV["RECAPTCHA_SECRET_KEY"]
-end
