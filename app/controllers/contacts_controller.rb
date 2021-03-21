86427c8c02e5df1f64b191386ce1b6d80bcc240c
Marie Leleu <marieleleu@icloud.com>
2021-03-21 16:30:15 +0100

    help

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,14 +6,12 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.deliver
+    if @contact.deliver &&  @contact[:email2].present?
       # flash.now[:error] = nil
       redirect_to root_path, notice: 'Message sent successfully'
     else
       flash.now[:error] = 'Cannot send message'
       render :new
-    end
-      redirect_to root_path if @contact[:email2].present?
     end
   end
 
